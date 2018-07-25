5b8568ee444003e0affe033f7c9d5a8d062a1575
freewind <nowindlee@gmail.com>
2018-07-25 17:20:34 +0800

    gc demo

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
-Java Hello World Demo
-=====================
+Java GC Demo
+============
 
-Run `Hello.java` in your IDE.
+How to trigger and view the GC information.
+
+In IDEA:
+--------
+
+Run `Hello` with `VM Options`:
+
+- Show GC Log: `-XX:+PrintGC`, or alias `-verbose:gc`
+- Specify a small heap: `-Xmx10m`
+
+Read the Log
+------------
+
+```
+[GC (System.gc())  3941K->664K(251392K), 0.0017948 secs]
+ 1^  2^^^^^^^^^^   3^^^^  4^^^ 5^^^^^^   6^^^^^^^^^^^^^
+```
+
+- 1: 可能值有GC/Full GC，表示当前这次GC是什么类型。应该大多数时候是GC
+- 2: 由`System.gc()`调用触发，其它可能值: `Allocation Failure`, `Ergonomics`
+- 3: 本次GC前，共占用heap 3941K
+- 4: 本次GC的heap占用变成了664K，少了很多
+- 5: GC之后，Head的总容量
+- 6: 本次GC所花时间
+
+Resources
+---------
+
+- <https://blog.codecentric.de/en/2014/01/useful-jvm-flags-part-8-gc-logging>
--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -1,9 +1,27 @@
 package demo;
 
+import java.util.Random;
+
 public class Hello {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws Exception {
+        while (true) {
+            generateBigArray();
+            if (System.currentTimeMillis() % 3 == 0) {
+                System.out.println("--- run gc manually ---");
+                System.gc();
+                // Runtime.getRuntime().gc();
+            }
+            Thread.sleep(1000);
+        }
+    }
+    private static void generateBigArray() {
+        int size = new Random().nextInt(50000);
+        Long[] array = new Long[size];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = System.currentTimeMillis();
+        }
+        System.out.println("# generate array with length: " + array.length);
     }
 
 }
